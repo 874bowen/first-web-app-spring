3851139d6dce0e0acd49f9cfcc5f1b914f90cc65
bowen <bowenivan16@gmail.com>
2022-05-27 16:42:26 +0300

    some service for logging in

---
+++ src/main/java/com/in28minutes/jee/UserValidationService.java
@@ -0,0 +1,9 @@
+package com.in28minutes.jee;
+
+public class UserValidationService {
+	public boolean isUserValid(String user, String password) {
+		if (user.equals("Bowen") && password.equals("secret"))
+			return true;
+		return false;
+	}
+}
